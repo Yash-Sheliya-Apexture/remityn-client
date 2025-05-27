f5ef58fa5d0b5bc0da18eff73e315467578b2b6f
Rudra-Apexture <rudra@apexture.in>
2025-05-27 18:11:44 +0530

    new design added

--- src/app/(website)/features/page.tsx
+++ src/app/(website)/features/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Features_Hero from "../components/features/Features_Hero";
-import FeaturesTab from "../components/features/FeaturesTab";
 
 const page = () => {
   return (
     <div className="Features-Main">
       <Features_Hero />
-      {/* <FeaturesTab /> */}
     </div>
   );
 };
